b320a146b93574b38614540b12dc1d1c405437ff
jaideepsrikakulapu <160497388+jaideepsrikakulapu@users.noreply.github.com>
2025-04-16 12:45:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
